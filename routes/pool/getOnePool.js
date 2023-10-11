a61c993557b85c60e733ab044a913c5a57de50e5
cheng-chun-yuan <83824908+cheng-chun-yuan@users.noreply.github.com>
2023-10-11 11:27:09 +0800

    watch:0.5-1

--- routes/pool/getOnePool.js
+++ routes/pool/getOnePool.js
@@ -3,7 +3,7 @@ import Pool from "../../models/Pool.js";
 const getPool = async (req, res) => {
     const { _id} = req.query;
     try {
-        const pool = await Pool.findByIdAndUpdate({_id},{$inc:{watches:0.5}}).catch((err)=>{console.log(err)});
+        const pool = await Pool.findByIdAndUpdate({_id},{$inc:{watches:1}}).catch((err)=>{console.log(err)});
         if (!pool) throw new Error("Pool not found");
         res.send(pool.getPublic());
     } catch (error) {
