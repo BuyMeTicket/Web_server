780b1baad05c61db647801434a8c828a1b80ccbc
cheng-chun-yuan <83824908+cheng-chun-yuan@users.noreply.github.com>
2023-10-19 02:03:13 +0800

    imagee_to_ipfs

--- routes/activity/addActivity.js
+++ routes/activity/addActivity.js
@@ -5,7 +5,7 @@ const addActivity = async (req, res) => {
     let tickets = JSON.parse(req.body.tickets);
     tickets = tickets.map((ticket, index)=>({...ticket, totalAmount:parseInt(ticket.totalAmount)}));
     const totalTickets = tickets.reduce((acc,ticket)=>acc+ticket.totalAmount,0);
-    const data = {...req.body, tickets, totalTickets };
+    const {data} = req.body;
     try {
         const newActivity =await new Activity({...data,totalTickets}).save().catch(e=>console.log("error in add activity",e));
         res.send('Activity added');
