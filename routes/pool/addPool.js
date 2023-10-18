2d4e3858682a00afed8074909103e9c915929d32
cheng-chun-yuan <83824908+cheng-chun-yuan@users.noreply.github.com>
2023-10-19 02:20:45 +0800

    imagee_to_ipfs

--- routes/pool/addPool.js
+++ routes/pool/addPool.js
@@ -1,7 +1,7 @@
 import Pool from "../../models/Pool.js";
 
 const addPool = async (req, res) => {
-    const data = {...req.body};
+    const data = req.body;
     try {
         const newPool = await new Pool(data).save().catch(e=>console.log("error in add pool",e));
         res.send('Pool added');
