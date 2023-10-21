5c831280caa5ad7477ca017a5052b761312bf785
cheng-chun-yuan <83824908+cheng-chun-yuan@users.noreply.github.com>
2023-10-22 01:38:46 +0800

    delete_one

--- routes/ticket/deleteTicket.js
+++ routes/ticket/deleteTicket.js
@@ -1,18 +1,11 @@
 import Ticket from "../../models/Ticket.js";
 
 const deleteTicket = async (req, res) => {
-    const { id } = req.params;
-    try {
-        // Check if the id is valid
-        const existedTicket= await Ticket.findById(id);
-        if (!existedTicket) {
-            return res.status(404).json({ message: "Ticket not found!" });
-        }
-        // Delete the todo
-        await  Ticket.findByIdAndDelete(id);
-        return res.status(200).json({ message: "Ticket deleted successfully!" });
-    } catch (error) {
-        return res.status(500).json({ message: error.message });
+    const target = req.query;
+    const ticket = await Ticket.deleteMany({ ...target })
+    if (!ticket) {
+        res.status(404).send("Ticket not found");
+        return;
     }
     res.send('delete success');
 }
