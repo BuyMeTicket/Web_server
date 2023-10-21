456ce3dce89320884d2dbee145f7321490008c54
cheng-chun-yuan <83824908+cheng-chun-yuan@users.noreply.github.com>
2023-10-22 00:56:13 +0800

    delete_one

--- routes/ticket/deleteTicket.js
+++ routes/ticket/deleteTicket.js
@@ -1,8 +1,8 @@
 import Ticket from "../../models/Ticket.js";
 
 const deleteTicket = async (req, res) => {
-    console.log(req.body.data);
-    const {_id:target} = req.body.data;
+    console.log(req.body);
+    const {_id:target} = req.body;
     const ticket = await Ticket.findByIdAndDelete(target);
     if (!ticket) {
         res.status(404).send("Ticket not found");
